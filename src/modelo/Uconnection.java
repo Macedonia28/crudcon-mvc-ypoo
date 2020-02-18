395fdf15286d9bedff2a20b2b52dc0f0c8dba738
alejandro <alejoagudelo23@gmail.com>
2020-02-18 13:23:25 -0500

    se crea la coneccion de la base de datos

---
+++ src/modelo/Uconnection.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Alejo
+ */
+public class Uconnection {
+ private static Connection con=null;
+ public static Connection getConnection(){
+     try{
+     if(con==null){
+      String usr="root";
+      String pswe="";
+      String url="jdbc:mysql://localhost:3306/usuarios";
+      con=DriverManager.getConnection(url,usr,pswe);
+      
+      }
+     return con;
+     }
+      catch(Exception ex){
+          ex.printStackTrace();
+          throw new RuntimeException(ex);
+      }
+     }
+ }
+
